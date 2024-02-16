61a273d63a18add46a4a25ef6a191e53ba5cbdea
Minty20090 <44348348+Minty20090@users.noreply.github.com>
2024-02-16 10:57:14 -0800

    k

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/agyro_Run_Using_Encoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/agyro_Run_Using_Encoders.java
@@ -164,6 +164,7 @@ public class agyro_Run_Using_Encoders extends LinearOpMode {
 
             while (opModeIsActive()) {
                 location = "Right";
+                location = "Right";
 
                 robot.clawL.setPosition(0);
                 sleep(500);
